f433b5d579bf649778c8d6faaa359db5174b49d1
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-27 20:59:18 +0530

    Update Destructuring_challenge.js

--- Days 1-10/Objects/Destructuring_challenge.js
+++ Days 1-10/Objects/Destructuring_challenge.js
@@ -3,3 +3,7 @@ const {name,room, weapons} = {"name":"Rusty","room":"kitchen", "weapons":"candle
 console.log(room)
 console.log(name)
 console.log(weapons)
+
+//this is what's happening :
+//obj =  {"name":"Rusty","room":"kitchen", "weapons":"candlestick"};
+//const name = obj.name  
