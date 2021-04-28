cc3a210872d4924a9d40d6ce714ba3bbe12be616
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-04-28 19:51:38 +0530

    Update _.map.js

--- Days 1-10/Objects/_.map.js
+++ Days 1-10/Objects/_.map.js
@@ -1,10 +1,10 @@
 //an array of broken weapons using _.map
 
 const _ = require("lodash"); 
-nst weapons =['candlestick', 'knife', 'gun'];
+const weapons =['candlestick', 'knife', 'gun'];
 
 const makeBroken = function(item) {
-	retunr `broken $(item)`;
+	return `broken $(item)`;
 	
 };
 
