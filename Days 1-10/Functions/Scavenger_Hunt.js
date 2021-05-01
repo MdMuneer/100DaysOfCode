47ccdee464e9689afff08538c96e5f8ca376ce4b
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-05-01 16:08:02 +0530

    Add files via upload

---
+++ Days 1-10/Functions/Scavenger_Hunt.js
@@ -0,0 +1,12 @@
+var nameImprover=function (name, adj){			//function defination //func name is nameImprover
+	return 'col ' + ' mc' + adj + ' pants';    //retrun `col $(name) mc $(adj) pants`;
+
+};
+
+$('body').hide();			//hide , for each, .on, .log , $ is a function 
+
+myArr.forEach(function(val){console.log(val);});
+
+$('button').on('click', function(){
+	console.log('Don\'t press my button!');
+});
