dbf396afda700f815be20cbc94f35eb80702947f
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-05-02 23:41:01 +0530

    Update array_like_object.js

--- Days 1-10/Functions/array_like_object.js
+++ Days 1-10/Functions/array_like_object.js
@@ -1,5 +1,5 @@
 const constructArr = function(){
-	const arr = Array.prototype.slice.call(arguments); //make array like object and make it an array //in es6 Array.from(arguments)
+	const arr = Array.prototype.slice.call(arguments); //make array like objects and make it an array //in es6 Array.from(arguments)
 	arr.push('the soocer room?');
 	return arr.join(' ');
 };
