8d4f9b28afcb1ba23d7fdf7fbb005291ed743595
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-05-10 17:51:23 +0530

    Add files via upload

---
+++ Days 11-20/DOM Manipulation/script.js
@@ -0,0 +1,25 @@
+// const body = document.body
+// const div = document.createElement("div")
+// div.innerText  = "Hello"
+// div.textContent= "Hello 2"
+// body.append(div) 
+
+// const div = document.querySelector('div')
+// console.log(div.textContent)
+// console.log(div.innerText)
+
+
+// const strong = document.createElement("strong")
+// strong.innerText="Hello new"
+// div.append(strong)
+
+
+const body = document.body
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#Hi")
+const spanBye = document.querySelector("#Bye")
+
+console.log(spanHi.getAttribute("id"))
+console.log(spanHi.id)
+
+// spanHi.remove()		
---
+++ Days 11-20/DOM Traversal/script.js
@@ -0,0 +1,10 @@
+// const grandparent = document.getElementById("grandparent-id");
+const grandparent = document.querySelector("#grandparent-id")
+
+changeColor(grandparent)
+
+function changeColor(element){
+	element.style.changeColor = "green"
+}
+
+
