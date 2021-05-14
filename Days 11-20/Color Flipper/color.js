d4ef5e5606634a79ef505734cea3bda724139545
Muhammad Muneer <31310562+MdMuneer@users.noreply.github.com>
2021-05-14 21:15:52 +0530

    Update color.js

--- Days 11-20/Color Flipper/color.js
+++ Days 11-20/Color Flipper/color.js
@@ -14,5 +14,5 @@ btn.addEventListener("click", function () {
 });
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * hex.length);
+  return Math.floor(Math.random() * hex.length); //rounding off the number using floor//
 }
